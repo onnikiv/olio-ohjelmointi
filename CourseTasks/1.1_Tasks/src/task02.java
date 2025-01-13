f4df74be53eb6ccfb171f3c9931e787ddd65d88a
onnik <onni.kivinen@gmail.com>
2025-01-13 18:34:36 +0200

    fininishing, + overkill

--- CourseTasks/1.1_Tasks/src/task02.java
+++ CourseTasks/1.1_Tasks/src/task02.java
@@ -1,8 +1,17 @@
 public class task02 {
     public static void main(String[] args) {
+        /*
         System.out.println("    *");
         System.out.println("   ***");
         System.out.println("  *****");
         System.out.println(" *******");
+        */
+
+        // Some overkill
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(" ".repeat((4 - i)) + "*".repeat(1 + i) + "*".repeat(i));
+        
+        }
     }
 }
+
