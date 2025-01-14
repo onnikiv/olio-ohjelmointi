72a33e8e28416ca73b43c61e3495e1fcd56c970b
onnik <onni.kivinen@gmail.com>
2025-01-14 13:10:17 +0200

    finished task01.java (Fahrenheit to Celsius).

--- CourseTasks/1.2_Tasks/src/task01.java
+++ CourseTasks/1.2_Tasks/src/task01.java
@@ -7,11 +7,9 @@ public class task01 {
 
         System.out.println("Give me Fahrenheit: ");
         double fahrenheit = scanner.nextDouble();
-
         double celsius = ((fahrenheit - 32) / 1.8);
 
         System.out.printf("In Celsius:%4.1f\n", celsius);
+        scanner.close();
     }
-
-
 }
