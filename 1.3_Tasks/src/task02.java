f8f4e1e3d7fb49130c47bbbc7eaffc5b78b47253
onnik <onni.kivinen@gmail.com>
2025-01-17 21:23:32 +0200

    finished binary to decimal converter // task02, module 1.3

--- 1.3_Tasks/src/task02.java
+++ 1.3_Tasks/src/task02.java
@@ -1,17 +1,20 @@
-
 import java.util.Scanner;
-
 
 public class task02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Enter a binarynumber: ");
+        System.out.print("\nEnter a Binary number: ");
+
         String binaryNumber = scanner.nextLine();
-
         int bitAmount = binaryNumber.length();
+        int decimalNumber = 0;
 
-        System.out.println("Number of bits: "+ bitAmount);
-    }
-
+        for (int i = 0; i < bitAmount; i++) {
+            int arvo = binaryNumber.charAt((bitAmount - 1) - i) - '0'; // ASCII-taulussa; charAt 0 = 48, charAt 1 = 49, siksi vähennetään char 0.
+            decimalNumber += arvo * Math.pow(2, i);}
+        
+        System.out.println("\nBinary number: " + binaryNumber + " , Bits: " + bitAmount + "\nDecimal number: " + decimalNumber+ "\n");
+        scanner.close();
+    } 
 }
