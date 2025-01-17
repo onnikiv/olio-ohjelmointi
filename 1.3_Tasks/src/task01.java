82e52e07e21f312355d41150199332dd8d8f6ad2
onnik <onni.kivinen@gmail.com>
2025-01-17 19:01:13 +0200

    finishing Quadratic Equation Solver task

---
+++ 1.3_Tasks/src/task01.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class task01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nax^2 + bx + c = 0");
+        System.out.println("Give coefficient (A):");
+        double a = scanner.nextDouble();
+
+        System.out.println("Give the coefficient B: ");
+        double b = scanner.nextDouble();
+
+        System.out.println("Give the coefficient C: ");
+        double c = scanner.nextDouble();
+
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+        double x1 = ((-b + Math.sqrt(discriminant)) / (2 * a));
+        double x2 = ((-b - Math.sqrt(discriminant)) / (2 * a));
+
+        System.out.println("\nEquation: " + a + "x^2 + " + b + "x + " + c + " = 0");
+        
+        if (a == 0) {
+            System.out.println("No solution, (A cannot be 0!\n");
+
+        } else if (discriminant == 0) {
+            System.out.println("Discriminant: " + discriminant);
+            System.out.println("Only one root");
+            System.out.println("Root: " + x1 + "\n");
+
+        } else if (discriminant > 0) {
+            System.out.println("Discriminant: " + discriminant);
+            System.out.println("Roots: ");
+            System.out.println("x_1: "+ x1);
+            System.out.println("x_2: "+ x2+ "\n");
+
+        } else {System.out.println("No real roots!\n");}
+
+        scanner.close();
+    }
+}
