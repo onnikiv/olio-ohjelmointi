5e8a023cfd3e2a3f23e9e40052db32f50a2468d6
onnik <onni.kivinen@gmail.com>
2025-01-20 12:21:06 +0200

    finishing

--- 1.3_Tasks/src/task03.java
+++ 1.3_Tasks/src/task03.java
@@ -6,27 +6,34 @@ public class task03 {
 
         System.out.println("Enter the Starting number: ");
         int startNumber = scanner.nextInt();
-        
+
         System.out.println("Enter the End number: ");
         int endNumber = scanner.nextInt();
-        
+
         if (startNumber >= 0 && endNumber > 0) {
-            System.out.println("good job");
+            System.out.println("Prime numbers: ");
 
             for (int i = startNumber; i <= endNumber; i++) {
-                
-                if (i % 2 == 0) {
-                    System.out.println(i + " is even");
-                } else {
-                    System.out.println(i + " is odd");
+                if (isPrime(i)) {
+                    System.out.print(i + ", ");
                 }
-                
             }
-
         } else {
             System.out.println("Both numbers must be positive!");
         }
 
+        scanner.close();
     }
 
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
