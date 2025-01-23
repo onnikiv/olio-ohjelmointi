600a971e013eed5d88f7812479b551e78fc620e9
onnik <onni.kivinen@gmail.com>
2025-01-23 19:21:38 +0200

    finish

--- 1.4_Tasks/src/task03.java
+++ 1.4_Tasks/src/task03.java
@@ -7,22 +7,36 @@ public class task03 {
         
         System.out.println("Enter the size of the array: ");
         int arraySize = scanner.nextInt();
+
+        // molemmat listat saman kokoisia
         int[] numberArray = new int[arraySize];
         int[] noDuplicatesArray = new int[arraySize];
-
-        for (int i = 0; i < arraySize; i ++) {
-            System.out.println("Enter integer " + (i+1) + ":");
-            numberArray[i] = scanner.nextInt();
-
-            
-        }
+        int noDuplicatesSize = 0;
 
         for (int i = 0; i < arraySize; i++) {
-            
+            System.out.println("Enter integer " + (i+1) + ":");
+            numberArray[i] = scanner.nextInt();
+        }
+        // 
+        for (int i = 0; i < arraySize; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < noDuplicatesSize; j++) {
+                if (numberArray[i] == noDuplicatesArray[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                noDuplicatesArray[noDuplicatesSize] = numberArray[i];
+                noDuplicatesSize++;
+            }
+        }
 
-            
+        System.out.println("Array without duplicates:");
+        for (int i = 0; i < noDuplicatesSize; i++) {
+            System.out.print(noDuplicatesArray[i] + " ");
         }
 
         scanner.close();
-}
+    }
 }
