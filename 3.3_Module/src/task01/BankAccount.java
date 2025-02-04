05852e2dcfbef77ac85ea5c6d20ab70b2c64e3d8
onnik <onni.kivinen@gmail.com>
2025-02-04 09:56:37 +0200

    jippii

--- 3.3_Module/src/task01/BankAccount.java
+++ 3.3_Module/src/task01/BankAccount.java
@@ -1,11 +1,41 @@
+package task01;
+
 public class BankAccount {
 
+    public static int totalAccounts = 0;
+    private int accountBalance;
+    private int accountNumber;
+
     // Your variable declarations and code here
-    public BankAccount() {
+    public BankAccount(int accountBalance) {
+        this.accountBalance = accountBalance;
+        this.accountNumber += totalAccounts;
+    }
+    
+    public int getAccountNumber() {
+        return totalAccounts;
     }
 
+    public double  getAccountBalance() {
+        return accountBalance;
+    }
 
-    
+    public void deposit(int money) {
+        this.accountBalance =+ money;
+    }
+
+    public void withdraw(int money) {
+        this.accountBalance =- money;
+    }
+
+    public int getBalance() {
+        return accountBalance;
+    }
+
+    public static int getTotalAccounts() {
+        return totalAccounts;
+    }
+
 
     public static void main(String[] args) {
         BankAccount account1 = new BankAccount(1000);
