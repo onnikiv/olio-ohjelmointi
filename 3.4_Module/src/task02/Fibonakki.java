b53a97db288051ad7ac66b231106b80d5e11ead7
onnik <onni.kivinen@gmail.com>
2025-02-06 16:51:21 +0200

    done // task02

--- 3.4_Module/src/task02/Fibonakki.java
+++ 3.4_Module/src/task02/Fibonakki.java
@@ -1,18 +1,44 @@
 package task02;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public class Fibonakki {
 
     public static void main(String[] args) {
         
         long num1 = 0;
         long num2 = 1;
-        long fibo;
-        
-        for (int i = 0; i <= 60; i++) {
-            System.out.println(i + ". " + num1);
-            fibo = num1 + num2;
-            num1 = num2;
-            num2 = fibo;
-        }
+        long fibo = num1;
+
+        try (Writer writer = new FileWriter("C:\\Users\\onnik\\Ohjelmointi\\olio-ohjelmointi\\3.4_Module\\src\\task02\\fibonacciValues.csv");
+                BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+                
+                // PUOSKAROINTIA -> 0 -> 1
+                bufferedWriter.write(String.valueOf(fibo + 1));
+                bufferedWriter.newLine();
+                // -------------
+                
+                // loopataan loput luvut
+               for (int i = 1; i < 60; i++) {
+
+                fibo = num1 + num2;
+                num1 = num2;
+                num2 = fibo;
+
+                bufferedWriter.write(String.valueOf(fibo));
+                bufferedWriter.newLine();
+            }
+   
+               bufferedWriter.flush();
+
+               System.out.println("Fibonakkiluvut tungettu csv-tiedostoon!");
+
+           } catch (IOException e) {
+            System.out.println("Ei iha toiminu");
+
+           }
     }
 }
