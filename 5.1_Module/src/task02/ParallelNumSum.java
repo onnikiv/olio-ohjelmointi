2bfda89e77c9aee46adfcf23729d2fb315f26c71
onnik <onni.kivinen@gmail.com>
2025-02-07 15:46:40 +0200

    done // task02

--- 5.1_Module/src/task02/ParallelNumSum.java
+++ 5.1_Module/src/task02/ParallelNumSum.java
@@ -15,17 +15,16 @@ public class ParallelNumSum extends Thread  {
     private final int startIndex;
     private final int endIndex;
 
-    private int threadSum = 0; // yksittäisen threadinSumma
+    private long threadSum = 0; // yksittäisen threadinSumma
     private static long totalSum = 0;
 
 
     public ParallelNumSum() {
         this.startIndex = nextStartIndex;
-        this.endIndex = (nextStartIndex + chunkSize) - 1; // -1 koska aloitetaan nollasta
+        this.endIndex = (nextStartIndex + chunkSize); // -1 koska aloitetaan nollasta
         nextStartIndex += chunkSize;
 
         System.out.println("UUSI THREAD - palankoko: " + chunkSize + ", välikkö: " + startIndex + "-" + endIndex);
-
     }
 
     @Override
@@ -39,9 +38,11 @@ public class ParallelNumSum extends Thread  {
 
 
     public static void main(String[] args) {
+        // harmillisesti tää ohjelma ei anna oikeata arvoa summalle, jos SIZE on pariton
+        // --> todo olisi viskata vaikka viimeiselle corelle se yksi ylimääräinen 
         
-        int ALKIOKOKO = 10; // 0-10 väliltä
-        SIZE = 1000; // Listan koko
+        int ALKIOKOKO = 100; // 0-100 väliltä
+        SIZE = 100000; // Listan koko
         numberArray = new int[SIZE];
 
         for (int i = 0; i < numberArray.length; i++) {
@@ -71,13 +72,17 @@ public class ParallelNumSum extends Thread  {
             } catch (InterruptedException e) {}}
 
         System.out.println("---------------------------------------");
-        System.out.println("Kokonaissumma: " + totalSum);
+        System.out.println("Kokonaissumma:  " + totalSum);
 
         long varmistus = 0;
-        
+
         for (int num : numberArray) {
             varmistus += num;
         }
-        System.out.println(varmistus);
+        System.out.println("Varmistussumma: "+ varmistus);
+        
+        if (totalSum == varmistus) {
+            System.out.println("jippii");
+        }
     }
 }
