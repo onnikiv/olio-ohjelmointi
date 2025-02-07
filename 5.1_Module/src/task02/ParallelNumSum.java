d7bdcfbbb74d2e61480e30f993b3009650ef2ed3
onnik <onni.kivinen@gmail.com>
2025-02-07 15:31:47 +0200

    melkein

--- 5.1_Module/src/task02/ParallelNumSum.java
+++ 5.1_Module/src/task02/ParallelNumSum.java
@@ -1,49 +1,83 @@
 package task02;
 
-public class ParallelNumSum extends Thread{
-    private final int amoutOfCores = Runtime.getRuntime().availableProcessors();
-    private final int SIZE = 1000;
-    private final int[] numberArray = new int[SIZE];
+import java.util.ArrayList;
 
-    private int threadSumPartSize;
+// SpagettiSoossi
 
-    
+public class ParallelNumSum extends Thread  {
+
+    private static int SIZE;
+    private static  int coreCount = Runtime.getRuntime().availableProcessors();
+    private static int[] numberArray = new int[SIZE];
+
+    private final int chunkSize = SIZE / coreCount;
+    private static int nextStartIndex = 0;
+    private final int startIndex;
+    private final int endIndex;
+
+    private int threadSum = 0; // yksittäisen threadinSumma
+    private static long totalSum = 0;
 
 
     public ParallelNumSum() {
-        this.threadSumPartSize = SIZE / amoutOfCores;
-        System.out.println("Thread: " + this.getName() + " osio: " + this.threadSumPartSize);
+        this.startIndex = nextStartIndex;
+        this.endIndex = (nextStartIndex + chunkSize) - 1; // -1 koska aloitetaan nollasta
+        nextStartIndex += chunkSize;
+
+        System.out.println("UUSI THREAD - palankoko: " + chunkSize + ", välikkö: " + startIndex + "-" + endIndex);
+
     }
 
-    
-
-
-    public void run() {
-
+    @Override
+    public void run() { //lasketaan jokaisen threadin osio
+        for (int i = startIndex; i < endIndex; i++) {
+            threadSum += numberArray[i];
+        }
+        totalSum += threadSum;
+        System.out.println(getName() + ", summa: " + threadSum + " -> lisätään kokosummaan.");
     }
 
 
     public static void main(String[] args) {
-
         
+        int ALKIOKOKO = 10; // 0-10 väliltä
+        SIZE = 1000; // Listan koko
+        numberArray = new int[SIZE];
 
-        // luodaan luvut listaan 0-1000 väliltä
-        /*
-         *     for (int i = 0; i < numberArray.length; i++) {
-            numberArray[i] = (int) (Math.random() * 1001);
-            System.out.print(numberArray[i] + ", "); // debug
+        for (int i = 0; i < numberArray.length; i++) {
+            numberArray[i] = (int) (Math.random() * (ALKIOKOKO+1));
+            System.out.print(numberArray[i] + ", ");
         }
-         */
-        int amoutOfCores = Runtime.getRuntime().availableProcessors();
+        System.out.println();
+        System.out.println("---------------------------------------");
+        
+        ArrayList<ParallelNumSum> threads = new ArrayList<>();
 
-        for (int i = 0; i < amoutOfCores; i++) {
-            ParallelNumSum thread = new ParallelNumSum();
-            thread.setName("thread" + (i + 1)); // oijjoi
-            
+        for (int i = 0; i < coreCount; i++) {
+            System.out.print(i + ". ");
+            threads.add(new ParallelNumSum());
         }
+        // jokane threadi käynnistetään
+        for (ParallelNumSum thread : threads) {
+            thread.start();}
 
+        System.out.println("---------------------------------------");
 
+        // pistetään ukot oottamaan toisiaan.
+        for (ParallelNumSum thread : threads) {
+            try {
+                thread.join();
 
+            } catch (InterruptedException e) {}}
+
+        System.out.println("---------------------------------------");
+        System.out.println("Kokonaissumma: " + totalSum);
+
+        long varmistus = 0;
+        
+        for (int num : numberArray) {
+            varmistus += num;
+        }
+        System.out.println(varmistus);
     }
-
 }
