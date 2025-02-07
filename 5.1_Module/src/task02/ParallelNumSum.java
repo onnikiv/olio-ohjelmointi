8d5abfe885feff10ded9a3118d86f9077602063f
onnik <onni.kivinen@gmail.com>
2025-02-07 12:42:55 +0200

    tweaks

--- 5.1_Module/src/task02/ParallelNumSum.java
+++ 5.1_Module/src/task02/ParallelNumSum.java
@@ -1,34 +1,49 @@
 package task02;
 
 public class ParallelNumSum extends Thread{
+    private final int amoutOfCores = Runtime.getRuntime().availableProcessors();
+    private final int SIZE = 1000;
+    private final int[] numberArray = new int[SIZE];
+
+    private int threadSumPartSize;
+
+    
+
+
+    public ParallelNumSum() {
+        this.threadSumPartSize = SIZE / amoutOfCores;
+        System.out.println("Thread: " + this.getName() + " osio: " + this.threadSumPartSize);
+    }
+
+    
+
+
+    public void run() {
+
+    }
 
 
     public static void main(String[] args) {
 
-        int SIZE = 100;
-        int[] numberArray = new int[SIZE];
+        
 
         // luodaan luvut listaan 0-1000 väliltä
-        for (int i = 0; i < numberArray.length; i++) {
+        /*
+         *     for (int i = 0; i < numberArray.length; i++) {
             numberArray[i] = (int) (Math.random() * 1001);
             System.out.print(numberArray[i] + ", "); // debug
         }
-
-        
+         */
         int amoutOfCores = Runtime.getRuntime().availableProcessors();
 
         for (int i = 0; i < amoutOfCores; i++) {
             ParallelNumSum thread = new ParallelNumSum();
             thread.setName("thread" + (i + 1)); // oijjoi
-
-            System.out.println(thread.getName()); // debug
             
         }
 
-        int threadOsio = (int)numberArray.length / amoutOfCores;
-        System.out.println(threadOsio);
 
-        
+
     }
 
 }
