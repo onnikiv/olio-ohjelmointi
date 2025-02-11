f508a24217f1760a1fe8040a0b9f37d7486aa534
onnik <onni.kivinen@gmail.com>
2025-02-11 18:37:19 +0200

    tweaks

--- 5.2_Module/src/task01/Customer.java
+++ 5.2_Module/src/task01/Customer.java
@@ -2,15 +2,38 @@ package task01;
 
 public class Customer extends Thread{
 
+    private int ticketsReserved;
     private int customerId;
-    private static int customerAmount;
+    private static int customerAmount = 0;
 
     public Customer() {
-        this.customerId = customerId;
+        this.customerId = (customerAmount + 1);
         customerAmount++;
+        this.ticketsReserved = 0;
     }
-    
 
+    public int getTotalAmountOfCustomers() {
+        return customerAmount;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void reserveTicket(int ticket) {
+        ticketsReserved += ticket;
+    }
+
+    public int getCustomerTicketAmount() {
+        return ticketsReserved;
+    }
+
+
+    public void printInfo() {
+        System.out.println("Customer ID: " + customerId);
+        System.out.println("Tickets: " + ticketsReserved);
+        
+    }
 
 
 }
--- 5.2_Module/src/task01/Main.java
+++ 5.2_Module/src/task01/Main.java
@@ -1,13 +1,23 @@
 package task01;
 
+
 public class Main {
 
     public static void main(String[] args) {
         
-
+    
         Customer customer1 = new Customer();
+        Customer customer2 = new Customer();
+        
 
-        Theater theater = new Theater(customer1);
+        System.out.println(customer1.getTotalAmountOfCustomers());
+
+        System.out.println(customer2.getCustomerId());
+        customer2.reserveTicket(6);
+
+        customer1.printInfo();
+        customer2.printInfo();
+
     }
 
 }
