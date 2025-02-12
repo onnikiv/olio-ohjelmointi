46c83306251fafea65e78c23674c9bdf20a9b8a2
onnik <onni.kivinen@gmail.com>
2025-02-12 09:36:41 +0200

    tweaks

--- 5.2_Module/src/task01/Main.java
+++ 5.2_Module/src/task01/Main.java
@@ -8,18 +8,23 @@ public class Main {
         
         ArrayList<Customer> customers = new ArrayList<>();
 
-        Theater teatteri = new Theater(30);
+        Theater teatteri = new Theater(10);
 
         for (int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
             Customer customer = new Customer(teatteri);
             customers.add(customer);
             
         }
+
+        for (Customer customer : customers) {
+            customer.start();
+
+            
+        }
         
         for (Customer customer : customers) {
             try {
-                System.out.print(customer.getCustomerId() + " - ");
-                customer.start();
+                // System.out.print(customer.getCustomerId() + " - ");
                 customer.join();
             } catch (InterruptedException e) {
 
