cae645399501fa916907b8b3c0a377c50cc36e9c
onnik <onni.kivinen@gmail.com>
2025-02-13 11:29:59 +0200

    babum

--- 5.2_Module/src/task02/ThreadSafety.java
+++ 5.2_Module/src/task02/ThreadSafety.java
@@ -2,6 +2,22 @@ package task02;
 
 import java.util.ArrayList;
 
+class ThreadClass extends Thread {
+
+    private final int threadId;
+    private static int threadCount = 0;
+
+    public ThreadClass() {
+        threadCount++;
+        this.threadId = threadCount;
+        System.out.println(threadId + " thread");
+    }
+
+    
+
+}
+
+
 public class ThreadSafety {
 
     private final ArrayList<String> elements;
@@ -11,15 +27,17 @@ public class ThreadSafety {
     
     }
 
-    public void addElement(String item) {
+    public synchronized void addElement(String item) {
         elements.add(item);
 
     }
 
-    public void removeElement(String item) {
+    public synchronized void removeElement(String item) {
         if (elements.contains(item)) {
             elements.remove(item);
+            System.out.println(item + " removed");
         }
+        System.out.println(item + "couldn't be removed");
     }
 
     public int getCollectionSize() {
@@ -28,6 +46,16 @@ public class ThreadSafety {
 
 
     public static void main(String[] args) {
+
+        ThreadSafety testings = new ThreadSafety();
+
+        System.out.println(testings.getCollectionSize());
+
+        ThreadClass thread1 = new ThreadClass();
+        
+        ThreadClass thread2 = new ThreadClass();
+
+
         
     }
 }
