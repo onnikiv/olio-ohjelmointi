07811f5f0dc259be924736d19cd86d832858a302
onnik <onni.kivinen@gmail.com>
2025-02-13 10:43:51 +0200

    twiiking

--- 5.2_Module/src/task02/ThreadSafety.java
+++ 5.2_Module/src/task02/ThreadSafety.java
@@ -1,5 +1,33 @@
 package task02;
 
+import java.util.ArrayList;
+
 public class ThreadSafety {
 
+    private final ArrayList<String> elements;
+
+    public ThreadSafety() {
+        this.elements = new ArrayList<>();
+    
+    }
+
+    public void addElement(String item) {
+        elements.add(item);
+
+    }
+
+    public void removeElement(String item) {
+        if (elements.contains(item)) {
+            elements.remove(item);
+        }
+    }
+
+    public int getCollectionSize() {
+        return elements.size();
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
 }
