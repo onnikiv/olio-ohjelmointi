b6a0deb81f2af41465b18157f04924900f64bf43
onnik <onni.kivinen@gmail.com>
2025-02-17 21:34:44 +0200

    tweaks

--- 6.1_Module/src/main/java/VirtualDictionary/Main.java
+++ 6.1_Module/src/main/java/VirtualDictionary/Main.java
@@ -3,7 +3,6 @@ package VirtualDictionary;
 import VirtualDictionary.View.DictionaryView;
 
 public class Main {
-
     public static void main(String[] args) {
         DictionaryView.launch(DictionaryView.class);
     }
