bd89f804624803ab428bdd8fb9d8d70d3478e868
onnik <onni.kivinen@gmail.com>
2025-03-06 14:49:45 +0200

    TODO: add currency method

---
+++ 7.3_Module/src/main/java/controller/CurrencyController.java
@@ -0,0 +1,111 @@
+package controller;
+
+import java.util.ArrayList;
+
+import dao.CurrencyDao;
+import entity.Currency;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class CurrencyController {
+
+    private ArrayList<Currency> currencies;
+
+    private CurrencyDao currencyDao;
+
+    @FXML
+    private TextField currencyAmount;
+
+    @FXML
+    private Text errorMessageText;
+
+    @FXML
+    private Text currencyNameFrom;
+
+    @FXML
+    private Text currencyNameTo;
+    
+    @FXML
+    private ChoiceBox<Object> initialCurrency;
+    
+    @FXML
+    private ChoiceBox<Object> convertToCurrency;
+
+
+    public void initialize() {
+        
+        try {
+            currencyDao = new CurrencyDao();
+            currencies = new ArrayList<>();
+            populateChoiceBoxes();
+
+            // handlerit -> saadaan valuutan nimi textiboxeihin
+            initialCurrency.setOnAction(event -> setCurrencyNameToBox());
+            convertToCurrency.setOnAction(event -> setCurrencyNameToBox());
+            
+            errorMessageText.setText("Connection Succesful!\nCurrencies have been added.");
+            
+        } catch (Exception e) { errorMessageText.setText("Connection Failed!\nCouldn't fetch Currencies.");}
+    }
+
+    private void setCurrencyNameToBox() {
+        String fromCurrencyCode = (String) initialCurrency.getValue();
+        String toCurrencyCode = (String) convertToCurrency.getValue();
+        
+        for (Currency currency : currencies) {
+            if (currency.getCurrencyCode().equals(fromCurrencyCode)) {
+                currencyNameFrom.setText(currency.getCurrencyName());
+            }
+            if (currency.getCurrencyCode().equals(toCurrencyCode)) {
+                currencyNameTo.setText(currency.getCurrencyName());
+            }
+        }
+    }
+
+    public void populateChoiceBoxes() {
+        for (int i = 1; i <= currencyDao.findAll().size(); i++) {
+
+            Currency currency = currencyDao.find(i);
+            currencies.add(currency);
+
+            initialCurrency.getItems().add(currency.getCurrencyCode());
+            convertToCurrency.getItems().add(currency.getCurrencyCode());
+        }
+    }
+
+    public void convertCurrency() {
+        try {
+
+            double amount = Double.parseDouble(currencyAmount.getText()); 
+            
+            if (amount >= 0) {
+                
+                double from = 0;
+                double to = 0;
+
+                for (Currency currency : currencies) {
+                    if (currency.getCurrencyCode().equals(initialCurrency.getValue())) {
+                        from = currency.getConversionRate();
+                    }
+                    if (currency.getCurrencyCode().equals(convertToCurrency.getValue())) {
+                        to = currency.getConversionRate();
+                    }
+                }
+
+                double convertedAmount = amount * (to / from);
+                
+                if (from != 0 && to != 0) {
+                    errorMessageText.setText(String.format("%.2f %s equals to\n %.2f %s", amount, 
+                    initialCurrency.getValue(), convertedAmount, convertToCurrency.getValue()));
+                
+                } else {errorMessageText.setText("Please Select The Currencies");}
+
+            } else {errorMessageText.setText("Number Must Be Positive!.");}
+            
+        } catch (NumberFormatException e) {
+            errorMessageText.setText("Please Enter A Number.");
+        }
+    }
+}
---
+++ 7.3_Module/src/main/java/entity/Currency.java
@@ -0,0 +1,56 @@
+package entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="currencies")
+public class Currency {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name="currencyCode")
+    private String currencyCode;
+    
+    @Column(name="currencyName")
+    private String  currencyName;
+
+
+    private double conversionRate;
+
+    public Currency(int id, String currencyCode, String currencyName, double conversionRate) {
+        super();
+        this.id = id;
+        this.currencyCode = currencyCode;
+        this.currencyName = currencyName;
+        this.conversionRate = conversionRate;
+    }
+
+    public Currency() {
+
+    }
+    
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+    
+    public String getCurrencyName() {
+        return currencyName;
+    }
+    
+    public double getConversionRate() {
+        return conversionRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+}
