48bd5693331819f6058f06fc3f370bd6afe211e7
onnik <onni.kivinen@gmail.com>
2025-03-07 22:00:49 +0200

    init

---
+++ 8.2_Module/src/test/java/ShoppingCartTest.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ShoppingCartTest {
+
+    private static int TESTNUMBER;
+    
+    @BeforeAll
+    static void beforeAll() {
+        TESTNUMBER = 0;
+        System.out.println("**--- ALOITETAAN TESTAAMINEN --------**");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        TESTNUMBER += 1;
+        System.out.println("**--- ALOITETAAN SEURAAVA TESTI "+ TESTNUMBER + " ---**");
+
+    }
+
+    @Test
+    public void testAddItem() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRemoveItem() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCalculateTotal() {
+        fail("Not yet implemented");
+    }
+}
