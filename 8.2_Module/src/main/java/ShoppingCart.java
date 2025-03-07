c21e26af336346684446229333a3f00492df3b34
onnik <onni.kivinen@gmail.com>
2025-03-07 22:16:05 +0200

    metodeja

--- 8.2_Module/src/main/java/ShoppingCart.java
+++ 8.2_Module/src/main/java/ShoppingCart.java
@@ -1,3 +1,25 @@
+
+import java.util.HashMap;
+
 public class ShoppingCart {
+    private final HashMap<String, Double> shoppingCart;
+
+    public ShoppingCart() {
+        this.shoppingCart = new HashMap<>();
+    }
+
+    public void addItem(String item, double cost) {
+        shoppingCart.put(item, cost);
+
+    }
+
+    public int getItemCount() {
+        return shoppingCart.size();
+    }
+
+    public void removeItem(String item) {
+        shoppingCart.remove(item);
+    }
+    
 
 }
