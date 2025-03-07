f0e7a9bd7c5c6cc9a81454e130408d25816f0ef0
onnik <onni.kivinen@gmail.com>
2025-03-07 19:06:36 +0200

    PenTest // done

---
+++ 8.1_Module/src/main/java/Pen/Pen.java
@@ -0,0 +1,38 @@
+package Pen;
+
+public class Pen {
+    public enum Color {
+        RED("red"), GREEN("green"), BLUE("blue");
+        private final String color;
+        Color(String color) { this.color = color; };
+        @Override public String toString() { return color; }
+    }
+
+    private Color color;
+    private boolean capOn = true;
+
+    public Pen() {
+        this.color = Color.RED;
+    }
+
+    public Pen(Color color) {
+        this.color = color;
+    }
+
+    // Write code here
+    public void capOff() {
+        this.capOn = false;
+    }
+
+    public void capOn() {
+        this.capOn = true;
+    }
+
+    public String draw() {
+        return capOn ? "" : "Drawing " + color;
+    }
+
+    public void changeColor(Color color) {
+        this.color = capOn ? color : this.color;
+    }
+}
