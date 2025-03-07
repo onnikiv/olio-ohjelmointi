1140c481d5eca55aa1b9d856c5eb13386e2ae83a
onnik <onni.kivinen@gmail.com>
2025-03-07 17:26:43 +0200

    init TransactionDao

---
+++ 7.4_Module/src/main/java/dao/TransactionDao.java
@@ -0,0 +1,15 @@
+package dao;
+
+import entity.Transaction;
+import jakarta.persistence.EntityManager;
+
+public class TransactionDao {
+
+    public void persist(Transaction transaction) {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        em.getTransaction().begin();
+        em.persist(transaction);
+        em.getTransaction().commit();
+
+    }
+}
