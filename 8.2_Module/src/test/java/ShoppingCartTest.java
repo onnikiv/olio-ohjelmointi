c4a43d96996757e7dd50dddac3e91cc5542eedad
onnik <onni.kivinen@gmail.com>
2025-03-07 22:16:09 +0200

    tweaks

--- 8.2_Module/src/test/java/ShoppingCartTest.java
+++ 8.2_Module/src/test/java/ShoppingCartTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,19 +18,33 @@ public class ShoppingCartTest {
     void beforeEach() {
         TESTNUMBER += 1;
         System.out.println("**--- ALOITETAAN SEURAAVA TESTI "+ TESTNUMBER + " ---**");
-
-        ShoppingCart sCart = new ShoppingCart();
-
     }
 
     @Test
     public void testAddItem() {
-        fail("Not yet implemented");
-    }
+        ShoppingCart sCart = new ShoppingCart();
+        sCart.addItem("peruna",1);
+        assertEquals(1, sCart.getItemCount());
+
+
+        sCart.addItem("omena",2);
+        assertEquals(2, sCart.getItemCount());
+        
+    }   
 
     @Test
     public void testRemoveItem() {
-        fail("Not yet implemented");
+        ShoppingCart sCart = new ShoppingCart();
+        sCart.addItem("taateli",4);
+        sCart.addItem("maito", 1);
+        sCart.addItem("jauheliha", 5);
+
+        sCart.removeItem("taateli");
+        sCart.removeItem("maito");
+        sCart.removeItem("jauheliha");
+
+        assertEquals(0, sCart.getItemCount());
+
     }
 
     @Test
