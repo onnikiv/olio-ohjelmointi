1d34dbca9a816470ed604119560e385a91fadd2e
onnik <onni.kivinen@gmail.com>
2025-03-16 14:39:03 +0200

    done // task01

---
+++ 8.3_Module/src/main/java/task1/ArrayTask.java
@@ -0,0 +1,17 @@
+package task1;
+
+import java.util.Arrays;
+
+public class ArrayTask {
+    public static void main(String[] args) {
+        int[] numbers = {35, 45, 50, 79, 36, 
+                         48, 39, 91, 62, 71,
+                         93, 30, 66, 79, 80, 
+                         46, 34, 28, 57, 42};
+
+        System.out.println("Mean: " + Arrays.stream(numbers)
+                            .average()
+                            .orElse(0.0));
+                            
+    }
+}
